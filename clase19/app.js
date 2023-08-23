087e82439868d1dbeaf3e29c0c1a7c2ffea91f2a
LuzBasante <luzbasante1@gmail.com>
2023-08-22 23:40:02 -0300

    modifique app.js

--- clase19/app.js
+++ clase19/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import __dirname from './src/utils.js'
 import handlebars from 'express-handlebars'
+import session from 'express-session'
+import __dirname from './src/utils.js'
 import routers from './routers/app.routers.js'
+import MongoStore from 'connect-mongo'
 import mongoose from 'mongoose';
 //import productModel from './dao/models/products.model.js';
 
@@ -10,27 +12,37 @@ const app = express()
 const PORT = process.env.PORT || 8080
 
 
-app.use(express.static(`${__dirname}/public`))
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+try {
+    await mongoose.connect('mongodb+srv://luzbasante1:luzbasante1@backendlb.ye4s1zp.mongodb.net/')
+    
+} catch (error) {
+    console.log(error)
+}
+
+// Sessions usando Mongo Storage
+app.use(session({
+    store: MongoStore.create({
+        client: mongoose.connection.getClient(), 
+        ttl: 3600
+    }),
+    secret: 'CoderSessionsPass',
+    resave: true,
+    saveUninitialized: true
+}))
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static(`${__dirname}/public`));
 
 app.engine('handlebars', handlebars.engine())
 app.set('views', `${__dirname}/views`)
 app.set('view engine', 'handlebars')
 
-app.use('/', routers)
 
 
-
-try {
-    await mongoose.connect('mongodb+srv://luzbasante1:luzbasante1@backendlb.ye4s1zp.mongodb.net/')
-    console.log('DB -ecommerce- connected')
-
-
-} catch (error) {
-    console.log(error)
-}
+app.use('/', routerApp)
 
 app.listen(PORT, () => {console.log(`Server is active and running on port ${PORT}`)})
 
